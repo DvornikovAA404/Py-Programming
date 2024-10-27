efa8f9ad28101a582a39e41123ddaccd0e8fca7d
Avadik <advornikov0809@gmail.com>
2024-10-27 16:03:25 +0700

    Lab3 in process

--- lab3/z2.py
+++ lab3/z2.py
@@ -1,4 +1,6 @@
-import math
 s = input("Enter a string: ")
-s1 = s[math.ceil(len(s)/2):] + s[:math.ceil(len(s)/2)]
-print(s1)
+sp = s.split()
+if len(sp) == 2:
+    print(sp[1] + ' ' + sp[0])
+else:
+    print("Ошибка! Некорректное количество слов")
