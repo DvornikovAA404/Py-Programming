7b3eb49a86eebd9cb4b9a70088b7fd09871dd792
Avadik <advornikov0809@gmail.com>
2024-10-27 16:00:01 +0700

    Lab3 in process

--- lab3/z1.py
+++ lab3/z1.py
@@ -1,2 +1,9 @@
-s = input("Enter the address: ") # sibsutis*ru
-print(s.replace("*",".")) #sibsutis.ru
+s = input("Enter a text: ")
+i = s.split()
+print(i)
+x = len(i)
+a = []
+for o in range(x):
+   if i[o] != i[o-1]:
+       a.append(i[o])
+print(''.join([s+' ' for s in a]))
